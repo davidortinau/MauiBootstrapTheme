306800f23cdce341ed246b02cf01ee9b144519bb
David Ortinau <david.ortinau@microsoft.com>
2026-02-20 10:24:53 -0600

    Fix build: Remove invalid UseMauiReactorApp options
    - Revert to parameterless UseMauiReactorApp<T>()
    - The options overload is not available in Reactor.Maui v4 public API

--- samples/MauiBootstrapTheme.Sample.Reactor/MauiProgram.cs
+++ samples/MauiBootstrapTheme.Sample.Reactor/MauiProgram.cs
@@ -11,10 +11,7 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder
-            .UseMauiReactorApp<MainPage>(options => 
-            {
-                options.EnableDebugLogging = true;
-            })
+            .UseMauiReactorApp<MainPage>()
             .UseBootstrapTheme(options =>
             {
                 options.AddTheme<Themes.DefaultTheme>("default");
